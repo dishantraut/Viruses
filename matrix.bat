4a2140ecda58688dd7c36ee21fbb6d2813673454
dishantraut <63045109+dishantraut@users.noreply.github.com>
2020-06-07 01:14:25 +0530

    Harmless
    It will just allocate random 
    memory to a variable X
    and print it on to the 
    shell in green color at a 
    high speed

---
+++ matrix.bat
@@ -0,0 +1,12 @@
+@echo off
+color 0a
+:x
+echo %random% %random% %random% 
+%random% %random% %random% 
+%random% %random% %random% 
+%random% %random% %random% 
+%random% %random% %random% 
+%random% %random% %random% 
+%random% %random% %random% 
+%random% 
+goto x
